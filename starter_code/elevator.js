adee3d5b3a0ac35b3723c6f06af1b7d5b66ec070
Alvaro <echanovevaro@gmail.com>
2018-01-30 00:12:06 +0100

    cuerta iteración a medias

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,50 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = "up";
+    this.interval   =  0;
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.interval = setInterval(() => {
+      update();
+    }, 1000);
+   }
+  
+   stop() {
+    clearInterval (this.interval);
+  }
+  
+  update() { 
+    log();
+  }
+  
   _passengersEnter() { }
+
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  
+  floorUp() {
+    if(this.floor<this.MAXFLOOR ){
+      this.floor++;
+    }
+   }
+  
+   floorDown() {
+    if(this.floor>0){
+      this.floor--;
+    }
+   }
+
+  call(person) {
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person);
+   }
+
+  log() { 
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}` );
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,14 @@
 const Elevator = require('./elevator.js');
+
+const elevator = new Elevator();
+for(i = 0; i<10 ; i++){
+    
+    elevator.log();
+    elevator.floorUp();
+}
+for(i = 10; i>0 ; i--){
+    
+    elevator.log();
+    elevator.floorDown();
+}
+elevator.log();
